772b618d5bcb43bcae8e64509d88add2c8a85a7b
Nichole Courtney <nichole.a.courtney@gmail.com>
2022-10-16 12:36:14 -0600

    Update: Everything works

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { Colors } from "react-native-paper";
 
 function View1() {
   return (
@@ -14,7 +15,7 @@ function View1() {
         backgroundColor: "#55c6cf",
       }}
     >
-      <Text>Hello SER 423, my name is Nichole Courtney.</Text>
+      <Text>Hello SER 423! my name is Nichole Courtney.</Text>
     </View>
   );
 }
@@ -45,17 +46,15 @@ function App() {
             let iconName;
 
             if (route.name === "View1") {
-              iconName = focused
-                ? "ios-information-circle"
-                : "ios-information-circle-outline";
+              iconName = focused ? "ios-information-circle" : "arrow-back";
             } else if (route.name === "View2") {
-              iconName = focused ? "ios-list-box" : "ios-list";
+              iconName = focused ? "ios-information-circle" : "arrow-forward";
             }
 
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: "deeppink",
+          tabBarActiveTintColor: "dodgerblue",
           tabBarInactiveTintColor: "gray",
         })}
       >
