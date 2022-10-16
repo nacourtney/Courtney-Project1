a7601b9f706cef25f4f190172be1933da9fc4ef2
Nichole Courtney <nichole.a.courtney@gmail.com>
2022-10-16 11:36:41 -0600

    Update: Everything working just not in MVC format

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-// You can import Ionicons from @expo/vector-icons if you use Expo or
-// react-native-vector-icons/Ionicons otherwise.
 import * as React from "react";
 import { Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
-function HomeScreen() {
+function View1() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#55c6cf",
+      }}
+    >
+      <Text>Hello SER 423, my name is Nichole Courtney.</Text>
     </View>
   );
 }
 
-function SettingsScreen() {
+function View2() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#dec466",
+      }}
+    >
+      <Text>Thanks for using my app!</Text>
     </View>
   );
 }
@@ -32,23 +44,23 @@ function App() {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === "Home") {
+            if (route.name === "View1") {
               iconName = focused
                 ? "ios-information-circle"
                 : "ios-information-circle-outline";
-            } else if (route.name === "Settings") {
+            } else if (route.name === "View2") {
               iconName = focused ? "ios-list-box" : "ios-list";
             }
 
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: "tomato",
+          tabBarActiveTintColor: "deeppink",
           tabBarInactiveTintColor: "gray",
         })}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="View1" component={View1} />
+        <Tab.Screen name="View2" component={View2} />
       </Tab.Navigator>
     </NavigationContainer>
   );
